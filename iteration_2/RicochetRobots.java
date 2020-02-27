45af58518a3e9a80cacb469d2f95348c46ec4ad6
hos-elazab <51264243+hos-elazab@users.noreply.github.com>
2020-02-27 20:21:41 -0330

    New update after checking another group's code

--- iteration_2/RicochetRobots.java
+++ iteration_2/RicochetRobots.java
@@ -1,41 +1,46 @@
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-public class RicochetRobots extends JFrame{
+public class RichochetRobots extends JFrame
+{
 	
 	ArrayList<JButton> buttonList = new ArrayList<>();
     
-	public RicochetRobots() {
+    public RichochetRobots () 
+    {
 		JFrame mainFrame = new JFrame("Ricochet Robots");
 		mainFrame.setSize(800,800);
 		mainFrame.setLocationRelativeTo(null);
 		JPanel Ricochet = new JPanel();
-	    
-		
-		for(int i = 0; i < 256; i++) {
+        Icon box = new ImageIcon(new ImageIcon("resources/heart.png").getImage().getScaledInstance(10, 10, java.awt.Image.SCALE_SMOOTH));
+        
+        for(int i = 0; i < 256; i++) 
+        {
 			
-			JButton button = new JButton();
-			button.setIcon(new ImageIcon("C:\\Users\\taylo\\eclipse-workspace\\Ricochet Robots\\src\\Images\\Tile.png"));
+			JButton button = new JButton(box);
 		    buttonList.add(button);
-		    //buttonLayout.add(button);}
+		    
 		}
 		
 		
-		for(int i = 0; i < 256; i++) {
+        for(int i = 0; i < 256; i++) 
+        {
 		    Ricochet.add(buttonList.get(i));
 		}
 		Ricochet.setLayout(new GridLayout(16,16));
 		mainFrame.add(Ricochet);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
-		
-		
-	}
-	public static void main ( String [] args){
-		new RicochetRobots();
+			
+    }
+    
+
+    public static void main ( String [] args)
+    {
+		new RichochetRobots ();
 	
-	
-}}
+    } 
+}
