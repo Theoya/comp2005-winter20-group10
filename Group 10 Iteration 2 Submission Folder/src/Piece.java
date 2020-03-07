d9c5e2b19aef00e8205fbdef900045384961b555
Theoya <38409551+Theoya@users.noreply.github.com>
2020-03-06 21:57:58 -0330

    Add files via upload

---
+++ Group 10 Iteration 2 Submission Folder/src/Piece.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Piece {
+	
+	private int location;
+	private int player;
+	private int bid;
+	private int lowestBid = 100;
+	private int timeLog;
+	public Piece(int loc, int play) {
+		this.location = loc;
+		this.player = play;
+		
+	}
+	public void move(int i) {
+		
+		this.location = location+i;
+		
+	}
+	public int getLoc() {
+		return(location);
+	}
+	
+	public int getPlayer() {
+		return(player);
+	}
+	public int getBid() {
+		
+		return(this.bid);
+	}
+	public void makeBid(int scale, int timeLog) {
+		Random rand = new Random();
+		
+		this.bid = rand.nextInt(scale+1);
+		if (scale<=lowestBid) {
+			if (bid <= lowestBid) {this.bid = rand.nextInt(scale+1);}
+			}
+		else {
+			if (bid >= lowestBid) {this.bid = rand.nextInt(lowestBid+1);}
+		}
+		if (bid == 0) {this.bid+=1;}
+		this.lowestBid = bid;
+		if (timeLog >= 2) {this.timeLog = timeLog;}
+		else {this.timeLog = 0;}
+	}
+	public int getTimeLog() {
+		return(timeLog);
+	}
+}
